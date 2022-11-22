a19d842cf86c8cde5aa9de3cd3faaf9d81c59578
RIYAZ AHMAD <riyazahmad.online@gmail.com>
2022-11-22 09:38:46 +0530

    Update otp.controller.ts

--- src/controllers/otp.controller.ts
+++ src/controllers/otp.controller.ts
@@ -18,27 +18,29 @@ export const createOtp= async (req:Request,res:Response)=>{
             encoding:"base32",
             step:45
         })
+        
 const url= secretKey.otpauth_url || ""
-        qrcode.toDataURL(url, function(err, data_url) {
-            console.log("the  QR code url: ",data_url);
-           
+const qrCodeUrl= qrcode.toDataURL(url, function(err, data_url) {
+            return data_url
+          
             // Display this data URL to the user in an <img> tag
             // Example:
         
 
           })
-
+          
+console.log(qrCodeUrl)
         const mailOptionsSender = {
             to: email,
             subject: `OTP for email verification ${email}`,
-            otp:`your OTP is: ${otp}`
+            otp:`your OTP is: ${otp} and valid for 60 seconds only`
           };
       
           
           mailSenderFunction(mailOptionsSender);
 
         // const otpData = await otpServices.createOtp({email,phone})
-        res.send({"otp":otp,"secret":secretKey.base32})
+        res.send({"otp":otp,"secret":secretKey.base32,"qrCodeUrl":qrCodeUrl})
 
        console.log(secretKey)
     }
